df7283f8a604c3f698c048588d275d1e1b47ce62
Anjali <anjalitbaiju@gmail.com>
2018-03-23 11:10:56 +0530

    Server modified

--- ServerTcp.java
+++ ServerTcp.java
@@ -1,7 +1,9 @@
 class ServerTcp{
-public static void main(String[] args)
-{
-
-}
+    public static void main(String[] args)
+    {
+        String clientSentence;
+        String capitalizedSentence;
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+    }
 
 }
