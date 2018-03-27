102b36e745cbc374e352b9d5d44c9a3595f79dbd
ashly <elsyantonybabu@gmail.com>
2018-03-27 14:36:37 +0530

    Client modified

--- ClientTcp.java
+++ ClientTcp.java
@@ -2,5 +2,8 @@ import java.io.*;
 import java.net.*;
 public class ClientTcp
 {
-
+public static void main(String[] args) throws Exception
+  {
+     Socket sock = new Socket("127.0.0.1", 3000);
+     }
 }
