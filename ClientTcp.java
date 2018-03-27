2f9f66e89a01e7179b02438ecaba09ca90e74912
ashly <elsyantonybabu@gmail.com>
2018-03-27 14:39:20 +0530

    Client modified

--- ClientTcp.java
+++ ClientTcp.java
@@ -5,5 +5,10 @@ public class ClientTcp
 public static void main(String[] args) throws Exception
   {
      Socket sock = new Socket("127.0.0.1", 3000);
+     / reading from keyboard (keyRead object)
+     BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+  	 // sending to client (pwrite object)
+     OutputStream ostream = sock.getOutputStream(); 
+     PrintWriter pwrite = new PrintWriter(ostream, true);
      }
 }
