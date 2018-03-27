a67b7e6d5fee1e7c0f547c68b1c55434ad16785b
Anjali <anjalitbaiju@gmail.com>
2018-03-27 14:49:24 +0530

    Server modified

--- ServerTcp.java
+++ ServerTcp.java
@@ -4,6 +4,9 @@ class ServerTcp{
     public static void main(String[] args)throws Exception
     {
         ServerSocket sersock = new ServerSocket(3000);
+        System.out.println("Server  ready");
+        Socket sock = sersock.accept( );   
+        BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in)); 
     }
 
 }
