49efad65c0cf2f4c92b92e7f70f1321d0a8dddae
ashly <elsyantonybabu@gmail.com>
2018-03-27 14:43:15 +0530

    Client modified

--- ClientTcp.java
+++ ClientTcp.java
@@ -5,10 +5,15 @@ public class ClientTcp
 public static void main(String[] args) throws Exception
   {
      Socket sock = new Socket("127.0.0.1", 3000);
-     / reading from keyboard (keyRead object)
+     //reading from keyboard (keyRead object)
      BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
   	 // sending to client (pwrite object)
      OutputStream ostream = sock.getOutputStream(); 
      PrintWriter pwrite = new PrintWriter(ostream, true);
+      // receiving from server ( receiveRead  object)
+     InputStream istream = sock.getInputStream();
+     BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+ 
+     System.out.println("Start the chitchat, type and press Enter key");
      }
 }
